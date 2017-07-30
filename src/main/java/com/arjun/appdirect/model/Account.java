d72da414b5c0306cf7f9b155da8c09490662aa8a
Arjun Karnwal <arjunkarnwal@Air-de-Arjun.local>
2017-07-29 22:13:21 -0400

    Randome Test 6

--- src/main/java/com/arjun/appdirect/model/Account.java
+++ src/main/java/com/arjun/appdirect/model/Account.java
@@ -1,10 +1,16 @@
 package com.arjun.appdirect.model;
 
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessType;
+
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
+@XmlAccessorType(XmlAccessType.PROPERTY)
+@XmlRootElement(name = "account")
 public class Account {
     private Long accountIdentifier;
     public Long getAccountIdentifier() {
